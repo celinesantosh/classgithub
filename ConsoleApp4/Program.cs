f9a844a2f15c7954a2466109477a09174598c594
celinesantosh@gmail.com <celinesantosh@gmail.com>
2020-09-25 18:09:22 +0530

    importing project

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number of employees");
+            int n = int.Parse(Console.ReadLine());
+            emp[] array = new emp[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("enter the details");
+                int eno = int.Parse(Console.ReadLine());
+                string name = Console.ReadLine();
+                string designation = Console.ReadLine();
+                double salary = double.Parse(Console.ReadLine());
+                array[i] = new emp(eno, name, designation, salary);
+            }
+            string[] s = new string[n];
+             int m = 0;
+            foreach (emp obj in array)
+            { 
+                s[m] = obj.name;
+                m++;
+            }
+            Array.Sort(s);
+            for(int i = 0; i < s.Length; i++)
+            {
+
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (s[i].Equals(array[j].name))
+                    {
+                        array[j].display();
+                        break;
+                    }
+                }
+                
+                   
+            }
+        }
+    }
+}
---
+++ ConsoleApp4/emp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Globalization;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class emp
+    {
+       public  int eno;
+        public string name, designation;
+        double salary;
+        public emp(int eno, String name,string designation,double salary)
+        {
+            this.eno = eno;
+            this.name = name;
+            this.designation = designation;
+            this.salary = salary;
+
+
+        }
+        public void display()
+        {
+            Console.WriteLine("emp no=" + eno);
+            Console.WriteLine("name=" + name);
+            Console.WriteLine("designation=" + designation);
+            Console.WriteLine("salary=" + salary);
+        }
+
+    }
+}
